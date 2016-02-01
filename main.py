254b2398349b8b42040a6f06d1dffe314ab46d44
FakeNameSE <grantlycee@gmail.com>
2016-02-01 16:56:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 # Prey implementation in Python using PyGame
 
-import sys, pygame, random, math
+from __future__ import division # required in Python 2.7
 
-pygame.init()
+import sys
+import pygame
+import random
+import math
 
-size = width, height = 800, 600
-black = 0, 0, 0
+# === constants === (UPPER_CASE names)
 
-maxVelocity = 10
-numBoids = 50
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 
-# lists
-# This is a list of 'sprites.' Each block in the program is
-# added to this list. The list is managed by a class called 'Group.'
-prey_list = pygame.sprite.Group()
+BLACK = (0, 0, 0)
 
-# This is a list of every sprite. All blocks and the player block as well.
-all_sprites_list = pygame.sprite.Group()
+MAX_VELOCITY = 10
+NUM_BOIDS = 50
+
+BORDER = 25
+
+# === classes === (CamelCase names for classes / lower_case names for method)
 
 class Prey(pygame.sprite.Sprite):
+
     def __init__(self, x, y):
         super(Prey, self).__init__()
 
         # Load image as sprite
-        prey_image = pygame.image.load("ressources/img/prey.png").convert()
-        self.image = prey_image
+        self.image = pygame.image.load("ressources/img/prey.png").convert()
 
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
@@ -34,17 +36,23 @@ class Prey(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-        self.velocityX = random.randint(-10, 10) / 10.0
-        self.velocityY = random.randint(-10, 10) / 10.0
+#        self.velocityX = random.randint(-10, 10) / 10.0
+#        self.velocityY = random.randint(-10, 10) / 10.0
 
-    "Return the distance from another prey"
+        self.velocityX = random.randint(1, 10) / 10.0
+        self.velocityY = random.randint(1, 10) / 10.0
+
     def distance(self, prey):
+        '''Return the distance from another prey'''
+
         distX = self.rect.x - prey.rect.x
         distY = self.rect.y - prey.rect.y
+
         return math.sqrt(distX * distX + distY * distY)
 
-    "Move closer to a set of prey_list"
-    def moveCloser(self, prey_list):
+    def cohesion(self, prey_list):
+        '''Move closer to a set of prey_list'''
+
         if len(prey_list) < 1:
             return
 
@@ -67,10 +75,13 @@ class Prey(pygame.sprite.Sprite):
         self.velocityX -= (avgX / 100)
         self.velocityY -= (avgY / 100)
 
-    "Move with a set of prey_list"
-    def moveWith(self, prey_list):
+
+    def alignment(self, prey_list):
+        '''Move with a set of prey_list'''
+
         if len(prey_list) < 1:
             return
+
         # calculate the average velocities of the other prey_list
         avgX = 0
         avgY = 0
@@ -86,9 +97,11 @@ class Prey(pygame.sprite.Sprite):
         self.velocityX += (avgX / 40)
         self.velocityY += (avgY / 40)
 
-    "Move away from a set of prey_list. This avoids crowding"
-    def moveAway(self, prey_list, minDistance):
-        if len(prey_list) < 1: return
+    def seperation(self, prey_list, minDistance):
+        '''Move away from a set of prey_list. This avoids crowding'''
+
+        if len(prey_list) < 1:
+            return
 
         distanceX = 0
         distanceY = 0
@@ -96,16 +109,21 @@ class Prey(pygame.sprite.Sprite):
 
         for prey in prey_list:
             distance = self.distance(prey)
+
             if  distance < minDistance:
                 numClose += 1
                 xdiff = (self.rect.x - prey.rect.x)
                 ydiff = (self.rect.y - prey.rect.y)
 
-                if xdiff >= 0: xdiff = math.sqrt(minDistance) - xdiff
-                elif xdiff < 0: xdiff = -math.sqrt(minDistance) - xdiff
+                if xdiff >= 0:
+                    xdiff = math.sqrt(minDistance) - xdiff
+                elif xdiff < 0:
+                    xdiff = -math.sqrt(minDistance) - xdiff
 
-                if ydiff >= 0: ydiff = math.sqrt(minDistance) - ydiff
-                elif ydiff < 0: ydiff = -math.sqrt(minDistance) - ydiff
+                if ydiff >= 0:
+                    ydiff = math.sqrt(minDistance) - ydiff
+                elif ydiff < 0:
+                    ydiff = -math.sqrt(minDistance) - ydiff
 
                 distanceX += xdiff
                 distanceY += ydiff
@@ -116,68 +134,102 @@ class Prey(pygame.sprite.Sprite):
         self.velocityX -= distanceX / 5
         self.velocityY -= distanceY / 5
 
-    "Perform actual movement based on our velocity"
     def update(self):
-        if abs(self.velocityX) > maxVelocity or abs(self.velocityY) > maxVelocity:
-            scaleFactor = maxVelocity / max(abs(self.velocityX), abs(self.velocityY))
+        '''Perform actual movement based on our velocity'''
+
+        if abs(self.velocityX) > MAX_VELOCITY or abs(self.velocityY) > MAX_VELOCITY:
+            scaleFactor = MAX_VELOCITY / max(abs(self.velocityX), abs(self.velocityY))
             self.velocityX *= scaleFactor
             self.velocityY *= scaleFactor
 
         self.rect.x += self.velocityX
         self.rect.y += self.velocityY
 
-screen = pygame.display.set_mode(size)
+# === main === (lower_case names)
+
+# --- init ---
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+#screen_rect = screen.get_rect()
+
+# --- objects ---
+
+# lists
+# This is a list of 'sprites.' Each block in the program is
+# added to this list. The list is managed by a class called 'Group.'
+prey_list = pygame.sprite.Group()
+
+# This is a list of every sprite. All blocks and the player block as well.
+all_sprites_list = pygame.sprite.Group()
 
 # create prey_list at random positions
-for i in range(numBoids):
-    prey = Prey(random.randint(0, width), random.randint(0, height))
+for i in range(NUM_BOIDS):
+    prey = Prey(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT))
     # Add the prey to the list of objects
     prey_list.add(prey)
     all_sprites_list.add(prey)
 
-while 1:
+# --- mainloop ---
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+
+    # --- events ---
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+    # --- updates ---
 
     for prey in prey_list:
         closeBoids = []
         for otherBoid in prey_list:
-            if otherBoid == prey: continue
+            if otherBoid == prey:
+                continue
             distance = prey.distance(otherBoid)
             if distance < 200:
                 closeBoids.append(otherBoid)
 
-
-        prey.moveCloser(closeBoids)
-        prey.moveWith(closeBoids)
-        prey.moveAway(closeBoids, 20)
+        prey.cohesion(closeBoids)
+        prey.alignment(closeBoids)
+        prey.seperation(closeBoids, 20)
 
         # ensure they stay within the screen space
         # if we roubound we can lose some of our velocity
-        border = 25
-        if prey.rect.x < border and prey.velocityX < 0:
+        if prey.rect.x < BORDER and prey.velocityX < 0:
             prey.velocityX = -prey.velocityX * random.random()
-        if prey.rect.x > width - border and prey.velocityX > 0:
+        if prey.rect.x > SCREEN_WIDTH - BORDER and prey.velocityX > 0:
             prey.velocityX = -prey.velocityX * random.random()
-        if prey.rect.y < border and prey.velocityY < 0:
+        if prey.rect.y < BORDER and prey.velocityY < 0:
             prey.velocityY = -prey.velocityY * random.random()
-        if prey.rect.y > height - border and prey.velocityY > 0:
+        if prey.rect.y > SCREEN_HEIGHT - BORDER and prey.velocityY > 0:
             prey.velocityY = -prey.velocityY * random.random()
 
+    prey.update()
 
-    screen.fill(black)
-    # Calls update() method on every sprite in the list
-    all_sprites_list.update()
+    # --- draws ---
+
+    screen.fill(BLACK)
 
     # Draw all the spites
     all_sprites_list.draw(screen)
 
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
+
     pygame.time.delay(10)
-    # Limit to 60 frames per second
+    # Limit to 120 frames per second
     # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
-    clock.tick(60)
-    pygame.display.flip()
-    pygame.time.delay(10)
+    clock.tick(120)
+
+# --- the end ---
+pygame.quit()
+sys.exit()
