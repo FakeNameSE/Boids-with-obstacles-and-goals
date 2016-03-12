75730038cb7ea193057e8e3859ac0b248a47f6a0
FakeNameSE <grantlycee@gmail.com>
2016-03-12 16:21:37 -0600

    Fully functional with obstacles and goals

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Prey implementation in Python using PyGame
+# Boid implementation in Python using PyGame
 
 from __future__ import division # required in Python 2.7
 
@@ -8,28 +8,27 @@ import pygame
 import random
 import math
 
-# === constants === (UPPER_CASE names)
+# === constants ===
 
-SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 1024
-
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 900
 BLACK = (0, 0, 0)
+RED   = (255, 0, 0)
 
-MAX_PREY_VELOCITY = 10
+MAX_PREY_VELOCITY = 8
 MAX_PREDATOR_VELOCITY = 12
-NUM_PREY = 50
-NUM_PREDATORS = 10
-
+NUM_BOIDS = 50
+NUM_OBSTACLES = 15
 BORDER = 25
 
-# === classes === (CamelCase names for classes / lower_case names for method)
+# === classes ===
 
-class Prey(pygame.sprite.Sprite):
+class Boid(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
-        super(Prey, self).__init__()
+        super(Boid, self).__init__()
 
         # Load image as sprite
-        self.image = pygame.image.load("ressources/img/prey.png").convert()
+        self.image = pygame.image.load("ressources/img/boid.png").convert()
 
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
@@ -37,39 +36,36 @@ class Prey(pygame.sprite.Sprite):
         # Coordinates
         self.rect.x = x
         self.rect.y = y
-
-#        self.velocityX = random.randint(-10, 10) / 10.0
-#        self.velocityY = random.randint(-10, 10) / 10.0
-
+        
         self.velocityX = random.randint(1, 10) / 10.0
         self.velocityY = random.randint(1, 10) / 10.0
 
-    def distance(self, prey):
-        '''Return the distance from another prey'''
+    def distance(self, boid):
+        '''Return the distance from another boid'''
 
-        distX = self.rect.x - prey.rect.x
-        distY = self.rect.y - prey.rect.y
+        distX = self.rect.x - boid.rect.x
+        distY = self.rect.y - boid.rect.y
 
         return math.sqrt(distX * distX + distY * distY)
 
-    def move_closer(self, prey_list):
-        '''Move closer to a set of prey_list'''
+    def move_closer(self, boid_list):
+        '''Move closer to a set of boid_list'''
 
-        if len(prey_list) < 1:
+        if len(boid_list) < 1:
             return
 
-        # calculate the average distances from the other prey_list
+        # calculate the average distances from the other boid_list
         avgX = 0
         avgY = 0
-        for prey in prey_list:
-            if prey.rect.x == self.rect.x and prey.rect.y == self.rect.y:
+        for boid in boid_list:
+            if boid.rect.x == self.rect.x and boid.rect.y == self.rect.y:
                 continue
 
-            avgX += (self.rect.x - prey.rect.x)
-            avgY += (self.rect.y - prey.rect.y)
+            avgX += (self.rect.x - boid.rect.x)
+            avgY += (self.rect.y - boid.rect.y)
 
-        avgX /= len(prey_list)
-        avgY /= len(prey_list)
+        avgX /= len(boid_list)
+        avgY /= len(boid_list)
 
         # set our velocity towards the others
         distance = math.sqrt((avgX * avgX) + (avgY * avgY)) * -1.0
@@ -78,44 +74,44 @@ class Prey(pygame.sprite.Sprite):
         self.velocityY -= (avgY / 100)
 
 
-    def move_with(self, prey_list):
-        '''Move with a set of prey_list'''
+    def move_with(self, boid_list):
+        '''Move with a set of boid_list'''
 
-        if len(prey_list) < 1:
+        if len(boid_list) < 1:
             return
 
-        # calculate the average velocities of the other prey_list
+        # calculate the average velocities of the other boid_list
         avgX = 0
         avgY = 0
 
-        for prey in prey_list:
-            avgX += prey.velocityX
-            avgY += prey.velocityY
+        for boid in boid_list:
+            avgX += boid.velocityX
+            avgY += boid.velocityY
 
-        avgX /= len(prey_list)
-        avgY /= len(prey_list)
+        avgX /= len(boid_list)
+        avgY /= len(boid_list)
 
         # set our velocity towards the others
         self.velocityX += (avgX / 40)
         self.velocityY += (avgY / 40)
 
-    def move_away(self, prey_list, minDistance):
-        '''Move away from a set of prey_list. This avoids crowding'''
+    def move_away(self, boid_list, minDistance):
+        '''Move away from a set of boid_list. This avoids crowding'''
 
-        if len(prey_list) < 1:
+        if len(boid_list) < 1:
             return
 
         distanceX = 0
         distanceY = 0
         numClose = 0
 
-        for prey in prey_list:
-            distance = self.distance(prey)
+        for boid in boid_list:
+            distance = self.distance(boid)
 
             if  distance < minDistance:
                 numClose += 1
-                xdiff = (self.rect.x - prey.rect.x)
-                ydiff = (self.rect.y - prey.rect.y)
+                xdiff = (self.rect.x - boid.rect.x)
+                ydiff = (self.rect.y - boid.rect.y)
 
                 if xdiff >= 0:
                     xdiff = math.sqrt(minDistance) - xdiff
@@ -135,25 +131,15 @@ class Prey(pygame.sprite.Sprite):
 
         self.velocityX -= distanceX / 5
         self.velocityY -= distanceY / 5
+        
+    def obstacle_avoidance(self, obstacle):
+        '''Avoid obstacles'''
+        self.velocityX += -1 * (obstacle.rect.x - self.rect.x) / 10
+        self.velocityY += -1 * (obstacle.rect.y - self.rect.y) / 10
 
-    def defend(self, predator_list):
-        nearest_predator = None
-        shortest_distance = None
-
-        for predator in predator_list:
-            distX = self.rect.x - predator.rect.x
-            distY = self.rect.y - predator.rect.y
-            d = distX*distX+distY*distY
-            if not shortest_distance or d < shortest_distance:
-                shortest_distance = d
-                nearest_predator = predator
-
-            # do something with nearest_prey, shortest_distance
-            trajectory_x = self.rect.x - nearest_predator.rect.x
-            trajectory_y = self.rect.y - nearest_predator.rect.y
-
-            self.velocityX += trajectory_x
-            self.velocityY += trajectory_y
+    def goal(self, mouse_x, mouse_y):
+        self.velocityX += (mouse_x - self.rect.x) / 100
+        self.velocityY += (mouse_y - self.rect.y) / 100
 
     def update(self):
         '''Perform actual movement based on our velocity'''
@@ -177,13 +163,15 @@ class Prey(pygame.sprite.Sprite):
         self.rect.x += self.velocityX
         self.rect.y += self.velocityY
 
-class Predator(pygame.sprite.Sprite):
+class Obstacle(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
-        super(Predator, self).__init__()
+        super(Obstacle, self).__init__()
 
         # Load image as sprite
-        self.image = pygame.image.load("ressources/img/predator.png").convert()
+        #self.image = pygame.image.load("ressources/img/predator.png").convert()
+        self.image = pygame.Surface([30, 30])
+        self.image.fill(RED)
 
         # Fetch the rectangle object that has the dimensions of the image
         self.rect = self.image.get_rect()
@@ -191,146 +179,9 @@ class Predator(pygame.sprite.Sprite):
         # Coordinates
         self.rect.x = x
         self.rect.y = y
-
-#        self.velocityX = random.randint(-10, 10) / 10.0
-#        self.velocityY = random.randint(-10, 10) / 10.0
-
-        self.velocityX = random.randint(1, 10) / 10.0
-        self.velocityY = random.randint(1, 10) / 10.0
-
-    def distance(self, predator):
-        '''Return the distance from another prey'''
-
-        distX = self.rect.x - predator.rect.x
-        distY = self.rect.y - predator.rect.y
-
-        return math.sqrt(distX * distX + distY * distY)
-
-    def move_closer(self, predator_list):
-        '''Move closer to a set of prey_list'''
-
-        if len(predator_list) < 1:
-            return
-
-        # calculate the average distances from the other prey_list
-        avgX = 0
-        avgY = 0
-        for predator in predator_list:
-            if predator.rect.x == self.rect.x and predator.rect.y == self.rect.y:
-                continue
-
-            avgX += (self.rect.x - predator.rect.x)
-            avgY += (self.rect.y - predator.rect.y)
-
-        avgX /= len(predator_list)
-        avgY /= len(predator_list)
-
-        # set our velocity towards the others
-        distance = math.sqrt((avgX * avgX) + (avgY * avgY)) * -1.0
-
-        self.velocityX -= (avgX / 100)
-        self.velocityY -= (avgY / 100)
-
-
-    def move_with(self, predator_list):
-        '''Move with a set of prey_list'''
-
-        if len(predator_list) < 1:
-            return
-
-        # calculate the average velocities of the other prey_list
-        avgX = 0
-        avgY = 0
-
-        for predator in predator_list:
-            avgX += predator.velocityX
-            avgY += predator.velocityY
-
-        avgX /= len(predator_list)
-        avgY /= len(predator_list)
-
-        # set our velocity towards the others
-        self.velocityX += (avgX / 40)
-        self.velocityY += (avgY / 40)
-
-    def move_away(self, predator_list, minDistance):
-        '''Move away from a set of prey_list. This avoids crowding'''
-
-        if len(prey_list) < 1:
-            return
-
-        distanceX = 0
-        distanceY = 0
-        numClose = 0
-
-        for predator in predator_list:
-            distance = self.distance(predator)
-
-            if  distance < minDistance:
-                numClose += 1
-                xdiff = (self.rect.x - predator.rect.x)
-                ydiff = (self.rect.y - predator.rect.y)
-
-                if xdiff >= 0:
-                    xdiff = math.sqrt(minDistance) - xdiff
-                elif xdiff < 0:
-                    xdiff = -math.sqrt(minDistance) - xdiff
-
-                if ydiff >= 0:
-                    ydiff = math.sqrt(minDistance) - ydiff
-                elif ydiff < 0:
-                    ydiff = -math.sqrt(minDistance) - ydiff
-
-                distanceX += xdiff
-                distanceY += ydiff
-
-        if numClose == 0:
-            return
-
-        self.velocityX -= distanceX / 5
-        self.velocityY -= distanceY / 5
-
-
-    def attack(self, prey_list):
-        nearest_prey = None
-        shortest_distance = None
-
-        for prey in prey_list:
-            distX = self.rect.x - prey.rect.x
-            distY = self.rect.y - prey.rect.y
-            d = distX*distX+distY*distY
-            if not shortest_distance or d < shortest_distance:
-                shortest_distance = d
-                nearest_prey = prey
-
-            # do something with nearest_prey, shortest_distance
-            trajectory_x = self.rect.x - nearest_prey.rect.x
-            trajectory_y = self.rect.y - nearest_prey.rect.y
-
-            self.velocityX -= trajectory_x
-            self.velocityY -= trajectory_y
-
+        
     def update(self):
-        '''Perform actual movement based on our velocity'''
-
-        # ensure they stay within the screen space
-        # if we roubound we can lose some of our velocity
-        if self.rect.x < BORDER and self.velocityX < 0:
-            self.velocityX = -self.velocityX * random.random()
-        if self.rect.x > SCREEN_WIDTH - BORDER and self.velocityX > 0:
-            self.velocityX = -self.velocityX * random.random()
-        if self.rect.y < BORDER and self.velocityY < 0:
-            self.velocityY = -self.velocityY * random.random()
-        if self.rect.y > SCREEN_HEIGHT - BORDER and self.velocityY > 0:
-            self.velocityY = -self.velocityY * random.random()
-
-        if abs(self.velocityX) > MAX_PREDATOR_VELOCITY or abs(self.velocityY) > MAX_PREDATOR_VELOCITY:
-            scaleFactor = MAX_PREDATOR_VELOCITY / max(abs(self.velocityX), abs(self.velocityY))
-            self.velocityX *= scaleFactor
-            self.velocityY *= scaleFactor
-
-        self.rect.x += self.velocityX
-        self.rect.y += self.velocityY
+        pass
 
 # === main === (lower_case names)
 
@@ -338,32 +189,31 @@ class Predator(pygame.sprite.Sprite):
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
+
+# Set the title of the window
+pygame.display.set_caption('Boids')
+
 #screen_rect = screen.get_rect()
 
 # --- objects ---
 
 # lists
-# This is a list of 'sprites.' Each block in the program is
-# added to this list. The list is managed by a class called 'Group.'
-prey_list = pygame.sprite.Group()
-predator_list = pygame.sprite.Group()
-
+boid_list = pygame.sprite.Group()
+obstacle_list = pygame.sprite.Group()
 # This is a list of every sprite. All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
 
-# create prey_list at random positions
-for i in range(NUM_PREY):
-    prey = Prey(random.randint(0, SCREEN_WIDTH - 100), random.randint(0, SCREEN_HEIGHT - 100))
-    # Add the prey to the list of objects
-    prey_list.add(prey)
-    all_sprites_list.add(prey)
+# create boids and obstacles at random positions on the screen
+for i in range(NUM_BOIDS):
+    boid = Boid(random.randint(0, SCREEN_WIDTH), random.randint(0, SCREEN_HEIGHT))
+    # Add the boid to the list of objects
+    boid_list.add(boid)
+    all_sprites_list.add(boid)
 
-# create predators at random positions
-for i in range(NUM_PREDATORS):
-    predator = Predator(random.randint(SCREEN_WIDTH - 100, SCREEN_WIDTH), random.randint(SCREEN_HEIGHT - 100, SCREEN_HEIGHT))
-    # Add the prey to the list of objects
-    predator_list.add(predator)
-    all_sprites_list.add(predator)
+for i in range(NUM_OBSTACLES):
+    obstacle = Obstacle(random.randint(0 + BORDER, SCREEN_WIDTH - BORDER), random.randint(0 + BORDER, SCREEN_HEIGHT - BORDER))
+    obstacle_list.add(obstacle)
+    all_sprites_list.add(obstacle)
 
 # --- mainloop ---
 
@@ -381,43 +231,35 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+    
+    pos = pygame.mouse.get_pos()
+    mouse_x = pos[0]
+    mouse_y = pos[1]
 
     # --- updates ---
-
-    for prey in prey_list:
-        closeprey = []
-        for otherprey in prey_list:
-            if otherprey == prey:
+    for boid in boid_list:
+        closeboid = []
+        obstacles = []
+        avoid = False
+        for otherboid in boid_list:
+            if otherboid == boid:
                 continue
-            distance = prey.distance(otherprey)
+            distance = boid.distance(otherboid)
             if distance < 200:
-                closeprey.append(otherprey)
-
-        prey.move_closer(closeprey)
-        prey.move_with(closeprey)
-        prey.move_away(closeprey, 20)
-        prey.defend(predator_list)
-        prey.update()
-
-    for predator in predator_list:
-        closepredator = []
-        for otherpredator in predator_list:
-            if otherpredator == predator:
-                continue
-            distance = predator.distance(otherpredator)
-            if distance < 200:
-                closepredator.append(otherpredator)
-
-        predator.move_closer(closepredator)
-        predator.move_with(closepredator)
-        predator.move_away(closepredator, 20)
-        predator.attack(prey_list)
-
-        predator.update()
-
-    # Calls update() method on every sprite in the list
-    #all_sprites_list.update()
-
+                closeboid.append(otherboid)
+        for obstacle in obstacle_list:
+            distance = boid.distance(obstacle)
+            if distance < 50:
+                avoid = True
+                
+        boid.move_closer(closeboid)
+        boid.move_with(closeboid)
+        boid.move_away(closeboid, 20)
+        if avoid == True:
+            boid.obstacle_avoidance(obstacle)
+        boid.goal(mouse_x, mouse_y)
+        boid.update()
+	
     # --- draws ---
 
     screen.fill(BLACK)
